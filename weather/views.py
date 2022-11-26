98da0b8a1315e72b799798a19bd2fd4716f16e36
Azam <abdurazakovazam01@gmail.com>
2022-11-26 15:48:55 +0500

    second commit

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,32 @@
+import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
 
 
 def index(request):
-    return render(request, 'weather/index.html', )
+    appid = '79b70a9b91346f56a5f21fdd1384dded'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+    if(request.method == "POST"):
+        form = CityForm(request.POST)
+        form.save()
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res['main']['temp'],
+            'icon': res['weather'][0]['icon']
+        }
+
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities, 'form': form}
+
+    return render(request, 'weather/index.html', context)
